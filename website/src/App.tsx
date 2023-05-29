23f3aac41067378b05b851f2ab3205b3afa9d111
Julia <jujucamp2003@gmail.com>
2023-05-29 19:41:02 -0300

    update

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Profile from './Components/Profile.js'
+import {textoNormal, textoTitulo, fundo} from './Components/stylesProfile'
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={fundo()}>
       <Profile/>
     </div>
   );
--- website/src/Components/Profile.js
+++ website/src/Components/Profile.js
@@ -1,25 +1,30 @@
 import React from 'react'
-import { Image } from 'antd'
+import { Image, Button } from 'antd'
+import { GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
+import { Typography } from 'antd'
+import {textoNormal, textoTitulo, fundo, imageProfile, imageBackground} from './stylesProfile.tsx'
 
-import { Typography } from 'antd';
 function Profile() {
     var name = "João Henri Carrenho Rocha"
     var description = " Essa é uma descrição do que ele faz e o que ele quer fazer e eu estou apenas simulando pq precisa ser um texto levemente maior por que sim"
     var graduação = "Ciência da Computação - USP"
     var status = "cursando  - 7º Semestre"
+    var imagePath = "https://avatars.githubusercontent.com/u/30930799?v=4"
+    var LinkedinButton = "\n Linkedin"
     const { Title } = Typography;
     return (
-      <div>
-      <Image src='../images/foto.png'/>
-       <Title level={3}>{name}</Title>
-       <Title level={5}>{description}</Title>
-       <Title level={4}>Graduação</Title>
-       <Title level={5}>{graduação}</Title>
-       <Title level={5}>{status}</Title>
-       <Title level={4}>Portifólio</Title>
-       curriculo + github
-       <Title level={4}>Contato</Title>
-       email linkedin facebook 
+      <div style={fundo()}>
+      <Image src={imagePath} style={imageProfile()} width={200} />
+      <Title level={3} style={textoTitulo()}>{name}</Title>
+      <Title level={5} style={textoNormal()}>{description}</Title>
+      <Title level={4} style={textoTitulo()}>Graduação</Title>
+      <Title level={5} style={textoNormal()}>{graduação}</Title>
+      <Title level={5} style={textoNormal()}>{status}</Title>
+      <Title level={4} style={textoTitulo()}>Portifólio</Title>
+      curriculo + github
+      <Title level={4} style={textoTitulo()}>Contato</Title>
+      <Button type="text" shape="circle" icon={<GithubOutlined/>} size={'large'}> Github</Button>
+      <Button type="text" shape="circle" icon={<LinkedinOutlined/>} size={'large'}> {LinkedinButton}</Button>
       </div>
     );
   }
